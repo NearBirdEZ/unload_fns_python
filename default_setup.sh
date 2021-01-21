09a2f822a31bc35f45e3fe448f82f761382d8215
NearBirdEZ <ytfhbbhl@yandex.ru>
2021-01-21 13:40:50 +0300

    add tkcalendar

--- default_setup.sh
+++ default_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ln -s /usr/bin/python3 /usr/bin/python
 add-apt-repository ppa:deadsnakes/ppa
 apt update && apt install python3.9
+ln -s /usr/bin/python3 /usr/bin/python
 echo "alias python3='python3.9'" >> ~/.bashrc
 apt install python3-pip && pip3 install -r requirements.txt
 apt install python3-tk
