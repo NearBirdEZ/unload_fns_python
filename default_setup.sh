231ef1c68fe4cdb7a48294b96b66186694acc56f
NearBirdEZ <ytfhbbhl@yandex.ru>
2021-02-02 18:25:23 +0300

    fix bash-script

--- default_setup.sh
+++ default_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
+ln -s /usr/bin/python3 /usr/bin/python &&
 add-apt-repository ppa:deadsnakes/ppa
 apt update && apt install python3.9
-update-alternatives --install /usr/bin/python3 python /usr/bin/python3.9 2
 apt install python3-pip && pip3 install -r requirements.txt
 apt install python3-tk
